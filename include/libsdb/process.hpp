f1fa03245b5b8f75ddc83d058e7826ba4fbdd8be
Kapy Kangombe <kapambwe@gmail.com>
2026-01-27 02:53:35 -0800

    Added automated tests for sdb::process class and implemented error communication between the debugger and the inferior process.

--- include/libsdb/process.hpp
+++ include/libsdb/process.hpp
@@ -16,6 +16,20 @@ namespace sdb {
     terminated
   };
 
+  enum class procfs_state : char {
+    Running        = 'R', // running or runnable
+    Sleeping       = 'S', // interruptible sleep
+    DiskSleep      = 'D', // uninterruptible sleep (usually IO)
+    Stopped        = 'T', // stopped by job control signal
+    TracingStop    = 't', // stopped by debugger (ptrace)
+    Zombie         = 'Z', // terminated, not reaped
+    Dead           = 'X', // dead (should never be seen)
+    Wakekill       = 'K', // wakekill (Linux-specific)
+    Waking         = 'W', // waking
+    Parked         = 'P', // parked (Linux-specific)
+    Idle           = 'I'  // idle kernel thread
+  };
+
   struct stop_reason {
     stop_reason(int wait_status);
 
@@ -26,7 +40,7 @@ namespace sdb {
   class process {
   public:
     ~process();
-    static std::unique_ptr<process> launch(std::filesystem::path path);
+    static std::unique_ptr<process> launch(std::filesystem::path path, bool debug = true);
     static std::unique_ptr<process> attach(pid_t pid);
 
     void resume();
@@ -39,13 +53,16 @@ namespace sdb {
     pid_t pid() const { return pid_; }
     process_state state() const { return state_; }
 
+    static constexpr char to_char(procfs_state s) noexcept { return static_cast<char>(s); }
+
   private:
-    process(pid_t pid, bool terminate_on_end)
-      : pid_(pid), terminate_on_end_(terminate_on_end) {}
+    process(pid_t pid, bool terminate_on_end, bool is_attached)
+      : pid_(pid), terminate_on_end_(terminate_on_end), is_attached_(is_attached) {}
 
     pid_t pid_ = 0;
     bool terminate_on_end_ = true;
     process_state state_ = process_state::stopped;
+    bool is_attached_ = true;
   };
 }
 
--- test/tests.cpp
+++ test/tests.cpp
@@ -1,5 +1,75 @@
 #include <catch2/catch_test_macros.hpp>
+#include <libsdb/process.hpp>
+#include <libsdb/error.hpp>
 
-TEST_CASE("validate environment") {
-  REQUIRE(true);
+#include <sys/types.h>
+#include <signal.h>
+
+#include <fstream>
+
+using namespace sdb;
+
+namespace {
+  bool process_exists(pid_t pid) {
+    auto ret = kill(pid, 0);
+    return ret != -1 && errno != ESRCH;
+  }
+
+  char get_process_status(pid_t pid) {
+    std::ifstream stat("/proc/" + std::to_string(pid) + "/stat");
+    std::string data;
+    std::getline(stat, data);
+    auto index_of_last_parenthesis = data.rfind(')');
+    auto index_of_status_indicator = index_of_last_parenthesis + 2;
+    return data[index_of_status_indicator];
+  }
 }
+
+TEST_CASE("process::launch success", "[process]") {
+  auto proc = process::launch("yes");
+  REQUIRE(process_exists(proc->pid()));
+}
+
+TEST_CASE("process::launch no such program", "[process]") {
+  REQUIRE_THROWS_AS(process::launch("test_non_existent_program_name"), error);
+}
+
+TEST_CASE("process::attach success", "[process]") {
+  auto target = process::launch("targets/run_endlessly", false);
+  auto proc = process::attach(target->pid());
+  REQUIRE(get_process_status(target->pid()) == process::to_char(procfs_state::TracingStop));
+}
+
+TEST_CASE("process::attach invalid PID", "[process]") {
+  REQUIRE_THROWS_AS(process::attach(0), error);
+}
+
+TEST_CASE("process::resume success", "[process]") {
+  {
+    auto proc = process::launch("targets/run_endlessly");
+    proc->resume();
+    auto status = get_process_status(proc->pid());
+    auto success = status == process::to_char(procfs_state::Running) ||
+      status == process::to_char(procfs_state::Sleeping);
+    REQUIRE(success);
+  }
+
+  {
+    auto target = process::launch("targets/run_endlessly", false);
+    auto proc = process::attach(target->pid());
+    proc->resume();
+    auto status = get_process_status(proc->pid());
+    auto success = status == process::to_char(procfs_state::Running) ||
+      status == process::to_char(procfs_state::Sleeping);
+    REQUIRE(success);
+  }
+}
+
+TEST_CASE("process::resume already terminated", "[process]") {
+  auto proc = process::launch("targets/end_immediately");
+  proc->resume();
+  proc->wait_on_signal();
+  REQUIRE_THROWS_AS(proc->resume(), error);
+}
+
+
