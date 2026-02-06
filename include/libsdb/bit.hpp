58a99b4b4f0c73253321fffa600ccee4eb7421d0
Kapy Kangombe <kapambwe@gmail.com>
2026-02-06 06:03:32 -0800

    Automated tests for writing to and reading from registers.

--- include/libsdb/bit.hpp
+++ include/libsdb/bit.hpp
@@ -2,6 +2,9 @@
 #define SDB_BIT_HPP
 
 #include <cstring>
+#include <vector>
+#include <string_view>
+
 #include <libsdb/types.hpp>
 
 namespace sdb {
@@ -34,6 +37,14 @@ namespace sdb {
     byte64 ret{};
     std::memcpy(&ret, &src, sizeof(From));
     return ret;
+  }
+
+  inline std::string_view to_string_view(const std::byte* data, std::size_t size) {
+    return { reinterpret_cast<const char*>(data), size };
+  }
+
+  inline std::string_view to_string_view(const std::vector<std::byte>& data) {
+    return to_string_view(data.data(), data.size());
   }
 }
 
